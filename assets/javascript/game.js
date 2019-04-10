4687e26abd66f50438a0a4afc86c8017414cd005
Hinojos <nhinojos@deloitte.com>
2019-04-10 02:48:35 -0700

    refreshing code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,61 @@
+//pseudocode approach
+//computer selects random number
+//crystals are assigned random number
+//event listeners wait for click
+//counter adds the click according to random number
+//function determines if counter === crystals assigned random number for win
+//if counter is greater than random numbers then lose
+//reset chooses new random numbers
+
+//define initial variables
 var crystal1 = 0;
 var crystal2 = 0;
 var cystal3 = 0;
 var crystal4 = 0;
+var userScore1 = 0;
+var userScore2 = 0;
+var userScore3 = 0;
+var userScore4 = 0;
 
-// may not need these variables if I can tag them by their ID
 //function start game
 
-var gameSelection = 10;
-// $("#display2User").text(gameSelection);
-
 //this initial function displays random numbers
+var counter = 0;
+var userScore = 0;
+
 startGame();
+
 function startGame() {
-    //get element by ID that is the header and display a random number 
     displayRandom = Math.floor(Math.random() * 120) + 1;
     console.log(displayRandom);
-    $("#display2User").text(gameSelection);
+    $("#display2User").text(displayRandom); //get element by ID that the user will try to play against
 
-    var counter = 0;
-    var crystalcounter1 = 2;
+    //I am repeating myself, how can I be more efficient?
+    var crystalcounter1 = Math.floor(Math.random() * 120) + 19;
+    console.log(crystalcounter1);
 
-    $("#crystal1").on("click", function () {
-        counter += crystalcounter1;
+    var crystalcounter2 = Math.floor(Math.random() * 120) + 19;
+    console.log(crystalcounter2)
 
-        // Clicking the button triggers an alert message.
-        alert("You clicked a crystal" + counter + "times!");
+    var crystalcounter3 = Math.floor(Math.random() * 120) + 19;
+    console.log(crystalcounter3)
+
+    var crystalcounter4 = Math.floor(Math.random() * 120) + 19;
+    console.log(crystalcounter4)
+
+    var crys1 = $("#crystal1").on("click", function () {
+        userScore1 = counter += crystalcounter1;
+        return userScore1;
 
     });
 
+    var crys2 = $("#crystal2").on("click", function () {
+        userScore2 = counter += crystalcounter2;
+        return userScore2;
+    });
 
+    userScore = crys1 + crys2;
+    $("#userScore").text(userScore); // Clicking the button writes the score to the DOM
+    console.log("the total userscore is" + userScore);
 
-
-
-    //assign random numbers to the crystals
-    //call function that ensures the addition
-
-
-
-    function sumNumbers() {
-        //onclick add 
-
-
-
-    };
 };
